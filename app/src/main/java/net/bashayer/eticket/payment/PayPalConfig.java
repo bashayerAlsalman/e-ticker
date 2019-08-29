875ced5b0959ecd526c397cdda40c269883aa558
NouraKY <noura.k.alyahya@gmail.com>
2019-08-30 00:24:30 +0300

    add payment

---
+++ app/src/main/java/net/bashayer/eticket/payment/PayPalConfig.java
@@ -0,0 +1,7 @@
+package net.bashayer.eticket.payment;
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "ARyKvHVn6ew3ZJ08BIOKVTmb8QhjixWWSq_v22nbAK7qsdmr_wAeW1-iIaZU3IEq66-ReGA4TWWaFgR5";
+
+}
